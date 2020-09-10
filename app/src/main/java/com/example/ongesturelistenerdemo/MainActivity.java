eef0e979e028453128e10601ca3d83284ee2b92c
sjs <sjs@sonic.net>
2020-09-10 13:18:20 -0700

    needed onTouch method to work.

--- app/src/main/java/com/example/ongesturelistenerdemo/MainActivity.java
+++ app/src/main/java/com/example/ongesturelistenerdemo/MainActivity.java
@@ -5,14 +5,12 @@ import androidx.core.view.GestureDetectorCompat;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 
 // See: https://stuff.mit.edu/afs/sipb/project/android/docs/training/gestures/detector.html
 
 public class MainActivity extends AppCompatActivity {
-    private static final String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
 
     @Override
@@ -25,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         mDetector = new GestureDetectorCompat(this, new GestureListener());
         // Set the gesture detector as the double tap listener.
         mDetector.setOnDoubleTapListener(new DoubleTapListener());
+    }
+    // Gesture detector and double tap listeners must have this.
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        Log.d("MainActivity","onTouchEvent: " + event.toString());
+        boolean rv = mDetector.onTouchEvent(event);
+        Log.d("MainActivity","onTouchEvent: rv = " + rv);
+        // rv == true means upstream handled event.
+        return (rv) ? rv : super.onTouchEvent(event);
     }
 }
